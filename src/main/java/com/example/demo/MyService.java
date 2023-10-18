867ec05c016229eba7cdf9318f2956e6c93384de
Rafael Menegon <dev.rafaelmenegon@gmail.com>
2023-10-18 10:10:32 -0300

    feat: improve logic to array

--- src/main/java/com/example/demo/MyService.java
+++ src/main/java/com/example/demo/MyService.java
@@ -3,25 +3,40 @@ package com.example.demo;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Service
 public class MyService {
 
-    public Map<String, Object> convertKeysToSnakeCase(Map<String, Object> inputMap) {
+    public static Map<String, Object> convertKeysToSnakeCase(Map<String, Object> inputMap) {
         Map<String, Object> outputMap = new HashMap<>();
         for (Map.Entry<String, Object> entry : inputMap.entrySet()) {
             String snakeCaseKey = camelToSnakeCase(entry.getKey());
             Object value = entry.getValue();
             if (value instanceof Map) {
                 value = convertKeysToSnakeCase((Map<String, Object>) value);
+            } else if (value instanceof List) {
+                value = convertListItemsToSnakeCase((List<Object>) value);
             }
             outputMap.put(snakeCaseKey, value);
         }
         return outputMap;
     }
 
-    private String camelToSnakeCase(String camelCase) {
+    private static List<Object> convertListItemsToSnakeCase(List<Object> inputList) {
+        for (int i = 0; i < inputList.size(); i++) {
+            Object listItem = inputList.get(i);
+            if (listItem instanceof Map) {
+                inputList.set(i, convertKeysToSnakeCase((Map<String, Object>) listItem));
+            } else if (listItem instanceof List) {
+                inputList.set(i, convertListItemsToSnakeCase((List<Object>) listItem));
+            }
+        }
+        return inputList;
+    }
+
+    private static String camelToSnakeCase(String camelCase) {
         return camelCase.replaceAll("([a-z])([A-Z])", "$1_$2").toLowerCase();
     }
 }
